35ab346964dc7a3a57e230f5172bcfba86fccfd5
Hyunho NOH <hyunhodl@gmail.com>
2020-06-30 09:47:23 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+
+const app = express();
+
+app.listen(5000);
