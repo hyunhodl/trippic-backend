caf423009d5f5d5ef135ed8c1dee8520967ecda6
Hyunho NOH <hyunhodl@gmail.com>
2020-07-08 16:22:38 +0900

    hash password

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 const HttpError = require("../models/http-error");
@@ -33,10 +34,17 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    } catch (error) {
+        return next(error);
+    }
+
     const createdUser = new User({
         name,
         email,
-        password,
+        password: hashedPassword,
         image: req.file.path,
         places: [],
     });
@@ -64,8 +72,20 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
-    if (!existingUser || existingUser.password !== password) {
+    if (!existingUser) {
         const error = new HttpError("인증 실패", 401);
+        return next(error);
+    }
+
+    let isValidPassword = false;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (error) {
+        return next(error);
+    }
+
+    if (!isValidPassword) {
+        const error = new HttpError("잘못된 비밀번호입니다.", 401);
         return next(error);
     }
 
