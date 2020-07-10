8c38b672de3751a0d1687af570f0c1a7bba10048
Hyunho NOH <hyunhodl@gmail.com>
2020-07-10 23:50:41 +0900

    add port env

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ mongoose
     )
     .then(() => {
         console.log("DB 연결 성공");
-        app.listen(5000);
+        app.listen(process.env.PORT || 5000);
     })
     .catch(() => {
         console.log("DB 연결 실패");
